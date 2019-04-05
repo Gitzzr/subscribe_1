f519eeb0903655e1299a32349c156d71b5c31485
zzr <zzr@DESKTOP-1STASHV>
2019-04-05 14:29:50 +0800

    1

---
+++ src/main/java/com/example/subscribe_1/Subscribe1Application.java
@@ -0,0 +1,13 @@
+package com.example.subscribe_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Subscribe1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Subscribe1Application.class, args);
+	}
+
+}
